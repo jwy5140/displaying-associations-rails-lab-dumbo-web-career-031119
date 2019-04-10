ff8e64f5350f90d8c069440ec1cbdef743f4e7fd
jwy5140 <jwy5140@gmail.com>
2019-04-09 23:38:31 -0400

    Done.

---
+++ db/migrate/20190410031641_add_artist_i_ds_to_songs.rb
@@ -0,0 +1,5 @@
+class AddArtistIDsToSongs < ActiveRecord::Migration[5.0]
+  def change
+    add_column :songs, :artist_id, :integer
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Danny")
+Artist.create(name: "Eric")
+Artist.create(name: "Terence")
+Artist.create(name: "Chewy")
+
+Song.create(title: "Hit Me Baby One More Time", artist_id: 1)
+Song.create(title: "Poker Face", artist_id: 2)
+Song.create(title: "Gangnam Style", artist_id: 3)
+Song.create(title: "Ratty Ratticus Runs Rampant", artist_id: 4)
+Song.create(title: "Get Low", artist_id: 1)
